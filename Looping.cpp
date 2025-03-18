bcfae3d0ede313ddc2351138daba96952b2a7035
kiieeaa <kiyaadinda5@gmail.com>
2025-03-18 19:01:51 +0700

    srand

--- Looping.cpp
+++ Looping.cpp
@@ -43,3 +43,18 @@ cout << "Nilai n terakhir = " << n << endl;
 
 cout << "perulangan do while" << endl;
 
+srand(time(0));
+n = rand() % 10;
+cout << "Nilai awal n = " << n << endl;
+
+do
+{
+    cout << "Nilai n = " << n << endl;
+    n = rand() % 10;
+    perulanganDo++;
+
+} while (n < 7);
+cout << "Jumlah perulangan = " << perulanganWhile << endl;
+cout << "Nilai n terakhir = " << n << endl;
+}
+
